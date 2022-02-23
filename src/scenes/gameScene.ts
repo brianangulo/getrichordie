5ae06c7e151c8cc02d7393b31b04a5fe75b5de34
Brian Angulo <bangulo219@gmail.com>
2022-02-23 00:32:57 -0600

    feat: setting up game scene with some ground and background

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,28 +1,55 @@
-import Phaser from 'phaser';
+import BaseScene from './BaseScene';
+import { ImageAsset } from './types';
 
-export default class Game extends Phaser.Scene {
+enum AssetKeys {
+  GameBg = 'game-bg',
+  Floor = 'floor',
+}
+
+export default class Game extends BaseScene {
   constructor() {
     super('game');
   }
 
-  private centerX: number;
-  private centerY: number;
+  private imgAssets: ImageAsset[] = [
+    {
+      key: AssetKeys.GameBg,
+      url: 'Background.png',
+    },
+    {
+      key: AssetKeys.Floor,
+      url: 'tile.png',
+    }
+  ]
 
   init() {
-    this.centerX = Number(this.sys.game.config.width) / 2;
-    this.centerY = Number(this.sys.game.config.height) / 2;
+    this.loadWindowDimensions();
   }
 
-  preload() {}
+  preload() {
+    this.loadPath();
+    this.load.image(this.imgAssets);
+  }
 
   create() {
-    let placeholder = this.add.text(
-      this.centerX,
-      this.centerY,
-      'This is a game',
-      { fontSize: '22px' }
-    );
-    placeholder.setInteractive();
-    placeholder.on('pointerdown', () => this.scene.start('menu'));
+    // background image
+    const background = this.add.image(this.centerX, this.centerY, AssetKeys.GameBg);
+    background.scaleX = 1.5;
+    background.scaleY = 2.0;
+    // spreads out the ground
+    this.createGround();
+  }
+
+  private createGround = () => {
+    // in pixels
+    const groundTileSize = 32;
+    const tilesQuantity: number = Math.ceil(this.windowWidth / groundTileSize);
+    // looping till outside the screen
+    for (let index = 0; index <= tilesQuantity; index++) {
+      // where to place the tile
+      const tileXPos = index === 0 ? index : groundTileSize * index;
+      this.add.image(tileXPos, this.windowHeight - 15, AssetKeys.Floor);
+      this.add.image(tileXPos, this.windowHeight - 47, AssetKeys.Floor);
+    }
   }
 }
