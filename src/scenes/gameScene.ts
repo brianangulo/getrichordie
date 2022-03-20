c62ef30c09e967c5d2275fd2d5b8a7090e8eb402
Brian Angulo <bangulo219@gmail.com>
2022-03-20 13:42:31 -0500

    feat: adding a health bar and player health logic

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,6 +1,7 @@
 import BaseScene from './BaseScene';
-import { ImageAsset, Cursors, StaticGround } from './types';
+import { ImageAsset, Cursors, StaticGround, HealthBar } from './types';
 import Player from '../game_objects/Player';
+import Money from '../game_objects/Money';
 
 enum AssetKeys {
   GameBg = 'game-bg',
@@ -12,10 +13,13 @@ export default class Game extends BaseScene {
     super('game');
     this.playerInstance = new Player(this);
   }
+  public static readonly MAX_PLAYER_HEALTH = 1000;
+  public static readonly PLAYER_HEALTH_UPDATE_AMOUNT = 10;
+  private playerHealth: number = 1000;
   private playerInstance: Player;
   private cursors: Cursors;
   private ground: StaticGround;
-
+  private healthBar: HealthBar;
   private imgAssets: ImageAsset[] = [
     {
       key: AssetKeys.GameBg,
@@ -62,6 +66,60 @@ export default class Game extends BaseScene {
     this.ground.createMultiple(tileData);
   };
 
+  private increasePlayerHealth = () => {
+    if (
+      !(this.playerHealth + Game.PLAYER_HEALTH_UPDATE_AMOUNT >
+      Game.MAX_PLAYER_HEALTH)
+    ) {
+      this.playerHealth += Game.PLAYER_HEALTH_UPDATE_AMOUNT;
+    } else {
+      this.playerHealth = Game.MAX_PLAYER_HEALTH;
+    }
+  };
+
+  private decreasePlayerHealth = () => {
+    if (!(this.playerHealth - Game.PLAYER_HEALTH_UPDATE_AMOUNT < 0)) {
+      this.playerHealth -= Game.PLAYER_HEALTH_UPDATE_AMOUNT;
+    } else {
+      // game over
+      console.log('game over fool');
+    }
+  };
+
+  private preLoadMoneyAsset = () => {
+    this.load.spritesheet('money', 'Money.png', {
+      frameWidth: Money.FRAME_WIDTH,
+      frameHeight: Money.FRAME_HEIGHT,
+      spacing: Money.FRAME_SPACING,
+    });
+  };
+
+  private createPlayerHealthBar = (x: number, y: number, color: number) => {
+    //draw the bar
+    const bar = this.add.graphics();
+
+    //color the bar
+    bar.fillStyle(color, 1);
+
+    //fill the bar with a rectangle
+    bar.fillRect(0, 0, 200, 50);
+
+    //position the bar
+    bar.x = x;
+    bar.y = y;
+
+    //return the bar
+    return bar;
+  };
+  // it updates the health bar
+  private updateHealthBar(bar: HealthBar, percentage: number) {
+    //scale the bar
+    bar.scaleX = percentage / 100;
+  }
+
+  // makes money objects rain
+  private makeItRain = () => {};
+
   // engine methods
   init() {
     this.loadWindowDimensions();
@@ -72,6 +130,7 @@ export default class Game extends BaseScene {
     this.loadPath();
     this.load.image(this.imgAssets);
     this.playerInstance.loadPlayerSprites();
+    this.preLoadMoneyAsset();
   }
 
   create() {
@@ -82,9 +141,25 @@ export default class Game extends BaseScene {
     this.playerInstance.createPlayer(this.centerX, this.centerY, 2);
     // adds collision with the ground
     this.playerInstance.addPlayerCollider(this.ground);
+    // add money
+    const money = new Money(this, this.centerX, this.centerY, this.ground);
+    this.healthBar = this.createPlayerHealthBar(this.centerX, 50, 0xe74c3c);
+    // writing health bar verbiage
+    this.add.text(this.centerX, this.healthBar.y - 16, 'HEALTH', {
+      fontSize: '16px',
+      fontStyle: 'bold',
+      color: 'black',
+    })
   }
-
+  private timer: number = 0;
   update(time: number, delta: number) {
-      this.playerInstance.playerMovement(this.cursors);
+    this.playerInstance.playerMovement(this.cursors);
+    this.updateHealthBar(this.healthBar, 0.10 * this.playerHealth);
+    // decreasing player health on a timer
+    this.timer += delta;
+    while (this.timer > 500) {
+      this.decreasePlayerHealth();
+      this.timer -= 500;
+    }
   }
 }
--- src/scenes/types.ts
+++ src/scenes/types.ts
@@ -4,3 +4,4 @@ export type ImageAsset = Phaser.Types.Loader.FileTypes.ImageFileConfig;
 export type Cursors = Phaser.Types.Input.Keyboard.CursorKeys;
 export type PlayerGameObject = Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 export type StaticGround = Phaser.Physics.Arcade.StaticGroup;
+export type HealthBar = Phaser.GameObjects.Graphics;
