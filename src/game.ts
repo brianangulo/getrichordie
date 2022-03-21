7be45c0f8c20d355d0e6853eb47b4970f75ee054
Brian Angulo <bangulo219@gmail.com>
2022-03-20 21:25:32 -0500

    feat: adding game over screen to game and remving debug mode

--- src/game.ts
+++ src/game.ts
@@ -1,16 +1,16 @@
 import Phaser from 'phaser';
-import { MenuScene, Game } from './scenes';
+import { MenuScene, Game, GameOver } from './scenes';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: [MenuScene, Game],
+  scene: [MenuScene, Game, GameOver],
   physics: {
     default: 'arcade',
     arcade: {
       gravity: { y: 500 },
-      debug: true,
+      debug: false,
     },
   },
 };
